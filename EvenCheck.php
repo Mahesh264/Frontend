67a3c568e31748899fb61f7fe6850dcff6e7593d
Mahesh264 <35969978+Mahesh264@users.noreply.github.com>
2025-02-24 17:04:25 +0530

    Add files via upload

---
+++ EvenCheck.php
@@ -0,0 +1,11 @@
+<?php
+
+$x = 4;
+
+// $output = ($x % 2) == 0;
+
+$outpt = ($x % 2 == 0) ? "$x  is an even number" : "$x  is an odd number";
+
+echo $outpt;
+
+?>
---
+++ Operators.php
@@ -0,0 +1,13 @@
+<?php
+
+$a = 5;
+$b = 5;
+
+
+$output = $a == $b;
+
+echo $output;
+
+
+
+?>
